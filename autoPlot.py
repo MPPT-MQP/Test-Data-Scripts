0eb9f9ae5615d0f72fbea9118c062a9c46e6916e
Kyle Rabbitt <119811866+kyler26@users.noreply.github.com>
2025-03-23 23:15:34 -0400

    updates

--- autoPlot.py
+++ autoPlot.py
@@ -23,6 +23,7 @@ import os
 print("============\nMQP GRAPHING: \nEntire folder will be read and ALL CSV files will be converted into graphs."
 "\nGraphs will be placed in the graphs subdirectory in your specified path.\n\n")
 path = input("Enter folder path to CSV files: ")
+path = path.replace('\\', '/')
 
 userInput = input("Is this path correct? (y/n): " + path + " ")
 if userInput != 'y':
@@ -31,21 +32,20 @@ if userInput != 'y':
 
 
 filesToGraph = []
+fileNameList = []
 # return all files as a list
 for fileName in os.listdir(path):
      # check the files which are end with specific extension
     if fileName.endswith(".csv"):
         # Save path of all CSV files
         filesToGraph.append(path + "/" + fileName)
-
-resultsPath = path + "/Graphs/"
-# Check if subdir exists, if not create it
-if not os.path.exists(resultsPath): 
-    # then create it. 
-    os.makedirs(resultsPath)
+        fileNameList.append(fileName)
 
 
+
+i=0
 for csvFile in filesToGraph:
+    
     print("Creating graphs for: " + csvFile)
     df = pd.read_csv(csvFile, encoding='ISO-8859-1', sep=',', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13])
 
@@ -53,15 +53,32 @@ for csvFile in filesToGraph:
     plt.rc('font', size=20) #Set font size
     plt.rcParams["font.family"] = "Aptos" #Set font
 
+    #Create folder for graph results
+    CSVName = fileNameList[i].split(".csv")
+    resultsPath = path + "/Graphs/" + CSVName[0] + "/"
+    # Check if subdir exists, if not create it
+    if not os.path.exists(resultsPath): 
+        # then create it. 
+        os.makedirs(resultsPath)
 
-    # Column format
-    # Timestamp, PM1 (V), PM1(I), PM1(W), PM2 (V), PM2(I), PM2(W), PM3 (V), PM3(I), PM3(W), Temp (C), Light (W/m^2), Duty, Algorithm
+    #Filter name for graphs
+    GraphName = CSVName[0].split('__')
+    GraphName = GraphName[1]
+
+    #Create array of time that is in seconds instead of ms
+    x = df.iloc[:, 1] #X axis is col 1 (elapsed time)
+    xArray = np.array(x)
+    TimeSec = xArray / 1000
+
+
+    # Column format (starts at 0)
+    # Timestamp, Elapsed Time, PM1 (V), PM1(I), PM1(W), PM2 (V), PM2(I), PM2(W), PM3 (V), PM3(I), PM3(W), Temp (C), Light (W/m^2), Duty, Algorithm
     # plt.scatter(df.iloc[:, X], df.iloc[:, Y])
 
 
     """ Create Voltage vs current graph """
-    x = df.iloc[:, 1] #X axis is col 1
-    y = df.iloc[:, 2] #Y axis is col 2
+    x = df.iloc[:, 2] #X axis is col 2
+    y = df.iloc[:, 3] #Y axis is col 3
 
     # Trendline?
     # z = np.polyfit(x, y, 2)
@@ -82,17 +99,15 @@ for csvFile in filesToGraph:
     ax.set_ylim([0, 2.5])
     ax.set_xlim([0, 25])
 
-    SplitName = csvFile.split("__")
-    SplitName = SplitName[1].split(".")
-    SaveFileName = SplitName[0] + "- CvsV"
+    SaveFileName = GraphName + "- Current vs Voltage"
 
     fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
-
+    plt.close()
 
 
     """ Create power vs voltage graph """
-    x = df.iloc[:, 1] #X axis is col 0
-    y = df.iloc[:, 3] #Y axis is col 3
+    x = df.iloc[:, 2] #X axis is col 2
+    y = df.iloc[:, 4] #Y axis is col 4
 
     # Trendline?
     # z = np.polyfit(x, y, 2)
@@ -111,17 +126,15 @@ for csvFile in filesToGraph:
     plt.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
     # ax.set_ylim([0, 30])
 
-    SplitName = csvFile.split("__")
-    SplitName = SplitName[1].split(".")
-    SaveFileName = SplitName[0] + "- PvsV"
+    SaveFileName = GraphName + "- Power vs Voltage"
 
     fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
-
+    plt.close()
 
 
     """ Create power vs time graph """
-    x = df.iloc[:, 0] #X axis is col 0
-    y = df.iloc[:, 3] #Y axis is col 3
+    x = TimeSec
+    y = df.iloc[:, 4] #Y axis is col 4
 
     # Trendline?
     # z = np.polyfit(x, y, 2)
@@ -141,16 +154,45 @@ for csvFile in filesToGraph:
     plt.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
     ax.set_ylim([0, 30])
 
-    SplitName = csvFile.split("__")
-    SplitName = SplitName[1].split(".")
-    SaveFileName = SplitName[0] + "- PvsT"
+    SaveFileName = GraphName + "- Power vs Time"
 
     fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
+    """ Create power vs time graph + light over time """
+    x = TimeSec
+    y = df.iloc[:, 4] #Y axis is col 4 (POWER)
+    z = df.iloc[:, 12] #Y2 axis, is col 12 (LIGHT)
+
+    fig, ax = plt.subplots()
+    ax2 = ax.twinx()
+    Power, = ax.plot(x,y, c="#AC2B37", label = "Power")
+    ax.scatter(x,y, marker= "^", c="#AC2B37")
+    Light, = ax2.plot(x,z, c="#003f5c", label = "Light")
+    fig.set_size_inches(12, 8, forward=True)
+    ax.set_xlabel("Time (S)")
+    ax.set_ylabel("Power (W)")
+    ax2.set_ylabel("Light (W/m^2)")
+    plt.title("Power and Light vs Time")
+    ax.grid(which='major', linestyle='-', linewidth=1, alpha=0.8)
+    ax.minorticks_on()
+    ax2.minorticks_on()
+    ax.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
+    ax.set_ylim([0, 30])
+    ylim = ax2.get_ylim()
+    ylim = ylim[1] * (1.1)
+    ax2.set_ylim(bottom = 0, top = ylim)
+    # Function add a legend
+    plt.legend(handles = [Power, Light], loc="center left")
+
+    SaveFileName = GraphName + "- Power vs Time W-Light"
+
+    fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
+    plt.close()
 
     """ Create light vs time graph """
-    x = df.iloc[:, 0] #X axis is col 0
-    y = df.iloc[:, 11] #Y axis is col 3
+    x = TimeSec
+    y = df.iloc[:, 12] #Y axis is col 12
 
     # Trendline?
     # z = np.polyfit(x, y, 2)
@@ -169,10 +211,11 @@ for csvFile in filesToGraph:
     plt.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
     # ax.set_ylim([0, 1300])
 
-    SplitName = csvFile.split("__")
-    SplitName = SplitName[1].split(".")
-    SaveFileName = SplitName[0] + "- LightvsT"
+    SaveFileName = GraphName + "- Light vs Time"
 
     fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
+    plt.close()
+
+    i=i+1
 
 print("Graphs Created")
