543696ce4fd23f0c99e0350ae805b2d20ecfa04d
Kyle Rabbitt <119811866+kyler26@users.noreply.github.com>
2025-04-03 01:57:59 -0400

    add pwr vs time W/light & AofA colors

--- autoPlot.py
+++ autoPlot.py
@@ -6,6 +6,7 @@ from tkinter import filedialog
 from datetime import datetime
 from numpy.polynomial import Polynomial
 import os
+from dataclasses import dataclass
 
 # File Picker
 # Tk.Tk().withdraw() # prevents an empty tkinter window from appearing
@@ -47,7 +48,7 @@ i=0
 for csvFile in filesToGraph:
     
     print("Creating graphs for: " + csvFile)
-    df = pd.read_csv(csvFile, encoding='ISO-8859-1', sep=',', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13])
+    df = pd.read_csv(csvFile, encoding='ISO-8859-1', sep=',', usecols=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])
 
     # Global Text size and font config
     plt.rc('font', size=20) #Set font size
@@ -246,7 +247,154 @@ for csvFile in filesToGraph:
     plt.close()
 
 
+    if "TMP" in GraphName:
+        """ Create power vs time graph + temperature over time TMP ONLY****** """
+        x = TimeSec
+        y = df.iloc[:, 4] #Y axis is col 4 (POWER)
+        z = df.iloc[:, 11] #Y2 axis, is col 11 (TEMP)
 
+        fig, ax = plt.subplots()
+        ax2 = ax.twinx()
+        Power, = ax.plot(x,y, c="#AC2B37", label = "Power")
+        ax.scatter(x,y, marker= "^", c="#AC2B37")
+        Light, = ax2.plot(x,z, c="#003f5c", label = "Temperature")
+        fig.set_size_inches(12, 8, forward=True)
+        ax.set_xlabel("Time (S)")
+        ax.set_ylabel("Power (W)")
+        ax2.set_ylabel("Temperature (°C)")
+        plt.title("Power and Temperature vs Time")
+        ax.grid(which='major', linestyle='-', linewidth=1, alpha=0.8)
+        ax.minorticks_on()
+        ax2.minorticks_on()
+        ax.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
+        ax.set_ylim([0, 30])
+        ylim = ax2.get_ylim()
+        ylim = ylim[1] * (1.1)
+        ax2.set_ylim(bottom = 0, top = ylim)
+        # Function add a legend
+        plt.legend(handles = [Power, Light], loc="center left")
+
+        SaveFileName = GraphName + "- Power vs Time W-Temperature"
+
+        fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
+        plt.close()
+
+
+
+    # Define vars to represent algorithms
+    CV= 0
+    B= 1
+    PNO= 2
+    PNOV= 3
+    INC= 4
+    INCV= 5
+    PSO= 6
+    TMP= 7
+
+    startFlag = True
+    if "AofA" in GraphName:
+
+        # List to store all info for graphing sections
+        algoSections = []
+        # Create custom AofA graph with highted sections
+        """ Create power vs time graph + light over time """
+        x = TimeSec
+        y = df.iloc[:, 4] #Y axis is col 4 (POWER)
+        z = df.iloc[:, 12] #Y2 axis, is col 12 (LIGHT)
+
+        # Determine where to create the colors
+        algo = df.iloc[:, 14] #Algo is in col 14
+        oldAlgoNum = -1
+        algoNum = -1
+        for index, algoName in enumerate(algo):
+            algoName = algoName.strip()
+            match algoName:
+                case "CV":
+                    algoNum = CV
+                    color = "#007ab1"
+                case "B":
+                    algoNum = B
+                    color = "#637bc9"
+                case "PNO":
+                    algoNum = PNO
+                    color = "#ac73c9"
+                case "PNOV":
+                    algoNum = PNOV
+                    color = "#e967b1"
+                case "INC":
+                    algoNum = INC
+                    color = "#ff6685"
+                case "PSO":
+                    algoNum = PSO
+                    color = "#ff7f50"
+                case "TMP":
+                    algoNum = TMP
+                    color = "#ffa600"
+
+            if algoNum != oldAlgoNum:
+                if startFlag == True: #If just starting 
+                    # Algo has changed, record index value as a new start
+                    algoSections.append(dict(start = index, algo = algoName, color = color))
+                    oldAlgoNum = algoNum
+                    startFlag = False
+                else:
+                    # Algo has changed, record PREVIOUS index value as the end
+                    # grab last item on the dict and add the end index
+                    algoSections[-1]["end"] = index-1
+                    
+                    #Now deal with the current new start
+                    algoSections.append(dict(start = index, algo = algoName, color = color))
+                    oldAlgoNum = algoNum
+
+        fig, ax = plt.subplots()
+        ax2 = ax.twinx()
+        Power, = ax.plot(x,y, c="#AC2B37", label = "Power")
+        ax.scatter(x,y, marker= "^", c="#AC2B37")
+        Light, = ax2.plot(x,z, c="#003f5c", label = "Light")
+        fig.set_size_inches(12, 8, forward=True)
+        ax.set_xlabel("Time (S)")
+        ax.set_ylabel("Power (W)")
+        ax2.set_ylabel("Light (W/m^2)")
+        plt.title("Power and Light vs Time")
+        ax.grid(which='major', linestyle='-', linewidth=1, alpha=0.8)
+        ax.minorticks_on()
+        ax2.minorticks_on()
+        ax.grid(which='minor', linestyle='--', linewidth=0.5, alpha=0.8)
+        ax.set_ylim([0, 30])
+        ylim = ax2.get_ylim()
+        ylim = ylim[1] * (1.1)
+        ax2.set_ylim(bottom = 0, top = ylim)
+
+        # Add in custom shaded sections
+        algoHandles = []
+        listOfAlgos = []
+
+        # need to fix the last section to add the end point as the end of the recorded time
+        algoSections[-1]["end"] = -1 # set index to end of time array
+
+        for index, algoDict in enumerate(algoSections):
+            # check if algo name is in the list yet to plot them on the legend only once
+            label = "_nolegend_" # Label will be _nolegend_, preventing it from showing on the legend
+            if algoDict.get("algo") not in listOfAlgos:
+                label = algoDict.get("algo")
+                listOfAlgos.append(algoDict.get("algo")) # add name to list if it is unique
+                
+            # get x values in time array and color from algoDict and make a colored area with 10% transparency
+            algoHandles.append(ax.axvspan(TimeSec[algoDict.get("start")], TimeSec[algoDict.get("end")], facecolor=algoDict.get("color"),
+             alpha=0.1, label= label))
+
+
+        # Create seperate color legend
+        first_legend = ax.legend(handles = algoHandles, loc='best', framealpha = 0.5)
+        ax.add_artist(first_legend)
+
+        # Function add a legend
+        plt.legend(handles = [Power, Light], loc="best")
+
+        SaveFileName = GraphName + "- Power vs Time W-Light"
+
+        fig.savefig(resultsPath + SaveFileName + '.svg', dpi=fig.dpi, bbox_inches='tight', pad_inches=0.5)
+        plt.close()
 
     i=i+1
 
